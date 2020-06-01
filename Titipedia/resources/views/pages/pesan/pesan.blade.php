7f94558683a371683fd1fe6707903081016f3be1
Giovanni Tjahjono <ivangiovn@gmail.com>
2020-06-01 19:16:08 +0800

    Fixing chat background colour

--- Titipedia/resources/views/pages/pesan/pesan.blade.php
+++ Titipedia/resources/views/pages/pesan/pesan.blade.php
@@ -1,53 +1,40 @@
 @extends('layouts.pesanview')
 @section('content')
 <!-- =========================================================== -->
-<div class="container mt-4">
+<div class="container mt-4 direct-chat direct-chat-success">
     <div class="card  shadow-lg p-3 mb-5 bg-white rounded border-0">
         <div class="card-header bg-white">
             <h3><?= $user2->first()->name; ?></h3>
         </div>
         <div class="card-body">
-            <!-- Direct Chat -->
             <div class="box-body">
-                <!-- Conversations are loaded here -->
                 <div class="direct-chat-messages">
                     @foreach($pesan as $data)
-                    <!-- Message. Default to the left -->
                     @if ($data->id_pengirim === Auth::user()->id)
-                    <!-- Message. Default to the left -->
                     <div class="direct-chat-msg">
                         <div class="direct-chat-info clearfix">
                             <span class="direct-chat-name pull-left"><?= $user1->first()->name; ?></span>
                             <span class="direct-chat-timestamp pull-right">{{$data->waktu_kirim}}</span>
                         </div>
-                        <!-- /.direct-chat-info -->
                         <img class="direct-chat-img" src="{{ asset('photo_profile/'.Auth::user()->foto)}}" alt="Message User Image"><!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                             {{$data->isi_pesan}}
                         </div>
-                        <!-- /.direct-chat-text -->
                     </div>
-                    <!-- /.direct-chat-msg -->
                     @else
-                    <!-- Message to the right -->
                     <div class="direct-chat-msg right">
                         <div class="direct-chat-info clearfix">
                             <span class="direct-chat-name pull-right"><?= $user2->first()->name; ?></span>
                             <span class="direct-chat-timestamp pull-left">{{$data->waktu_kirim}}</span>
                         </div>
-                        <!-- /.direct-chat-info -->
                         <img class="direct-chat-img" src="{{ asset('photo_profile/'.Auth::user()->foto)}}" alt="Message User Image"><!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                             {{$data->isi_pesan}}
                         </div>
-                        <!-- /.direct-chat-text -->
                     </div>
                     @endif
-                    <!-- /.direct-chat-msg -->
                     @endforeach
                 </div>
-                <!--/.direct-chat-messages-->
-                <!-- /.direct-chat-pane -->
             </div>
             <!-- /.box-body -->
             <div class="box-footer">
